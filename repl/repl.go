c63d848bebf5e0f0d421cf5d886e47710ecb2dbe
Abhishek Sunil <abhisheksunil92@gmail.com>
2023-12-19 12:08:47 +0530

    add evaluator, objects and environment

--- repl/repl.go
+++ repl/repl.go
@@ -2,7 +2,9 @@ package repl
 
 import (
 	"bufio"
+	"falcon/evaluator"
 	"falcon/lexer"
+	"falcon/object"
 	"falcon/parser"
 	"fmt"
 	"io"
@@ -12,6 +14,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 	for {
 		//print prompt
 		fmt.Printf("%s", PROMPT)
@@ -31,9 +34,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		//print AST
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
